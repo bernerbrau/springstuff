30b37fa65c74a8d04c51cb2d5ba5aac5cc803908
bernerdc <bernerdc@b9cdbc32-ae21-0410-9a0e-be68b19515d5>
2017-03-20 20:15:46 +0000

    Making SVN checkout a local gradle plugin
    git-svn-id: https://vcs.mc.vanderbilt.edu/health-it/continuum/spring/trunk@24239 b9cdbc32-ae21-0410-9a0e-be68b19515d5

---
+++ buildSrc/src/main/groovy/org/vumc/continuum/SvnCheckout.groovy
@@ -0,0 +1,47 @@
+package org.vumc.continuum
+
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+import org.tmatesoft.svn.core.internal.wc.*
+import org.tmatesoft.svn.core.wc.*
+import org.tmatesoft.svn.core.*
+
+class SvnCheckout extends DefaultTask {
+    public String svnUrl
+    public String projectDir
+
+    @TaskAction
+    def run() {
+        def rev = SVNRevision.HEAD
+        def repoUrl
+        try {
+            repoUrl = SVNURL.parseURIEncoded(svnUrl)
+        } catch (SVNException e) {
+            throw new InvalidUserDataException("Invalid svnUrl value: $svnUrl", e)
+        }
+
+        def dir = new File(projectDir)
+        def performUpdate = false
+        if (dir.exists()) {
+            if (!dir.isDirectory()) {
+                throw new InvalidUserDataException("workspaceDir $dir.absolutePath must be a directory")
+            }
+            if (dir.list()) {
+                performUpdate = true
+            }
+        }
+        try {
+            def clientManager = SVNClientManager.newInstance(
+                    // create a local SVN config dir to make sure we don't reuse existing credentials:
+                    new DefaultSVNOptions(new File("${System.getProperty("user.home")}/.subversion"), false))
+
+            if (performUpdate) {
+                clientManager.updateClient.doUpdate(dir, rev, SVNDepth.INFINITY, false, false)
+            } else {
+                clientManager.updateClient.doCheckout(repoUrl, dir, SVNRevision.UNDEFINED, rev, SVNDepth.INFINITY, false)
+            }
+        } catch (SVNException e) {
+            throw new InvalidUserDataException((performUpdate ? "svn-update" : "svn-checkout") + " failed for $svnUrl\n" + e.message, e)
+        }
+    }
+}
