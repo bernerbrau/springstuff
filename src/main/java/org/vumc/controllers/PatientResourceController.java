c3a4813161fe71416f93ca173a8887129f86e2d7
bernerdc <bernerdc@b9cdbc32-ae21-0410-9a0e-be68b19515d5>
2017-04-05 17:38:33 +0000

    Reverting
    git-svn-id: https://vcs.mc.vanderbilt.edu/health-it/continuum/spring/trunk@24706 b9cdbc32-ae21-0410-9a0e-be68b19515d5

--- src/main/java/org/vumc/controllers/PatientResourceController.java
+++ src/main/java/org/vumc/controllers/PatientResourceController.java
@@ -1,37 +1,67 @@
+/* Project: continuum
+ * File: PatientResource.java
+ * Created: Mar 03, 2017
+ * Author: Derek Berner - derek.c.berner@vanderbilt.edu
+ *
+ * This code is copyright (c) 2017 Vanderbilt University Medical Center
+ */
 package org.vumc.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.vumc.model.Patient;
 import org.vumc.repository.PatientRepository;
+import rx.Observer;
 
-import java.util.List;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 
-@RepositoryRestController
+@RestController
 @RequestMapping("/api/patients")
-@ExposesResourceFor(Patient.class)
-public class PatientResourceController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PatientResourceController.class);
+public class PatientResourceController
+{
+  private static final Logger LOGGER = LoggerFactory.getLogger(PatientResourceController.class);
 
-    private final PatientRepository repository;
+  private final Observer<String> patientXMLObserver;
+  private final PatientRepository patientRepository;
 
-    @Autowired
-    public PatientResourceController(PatientRepository repository) {
-        this.repository = repository;
+  @Autowired
+  public PatientResourceController(
+      @Qualifier("patientXMLObserver") Observer<String> patientXMLObserver,
+      PatientRepository patientRepository)
+  {
+    this.patientXMLObserver = patientXMLObserver;
+    this.patientRepository = patientRepository;
+  }
+
+  @RequestMapping(path = "c32", method = RequestMethod.POST,
+                  consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE })
+  public void postC32Document(@RequestBody String inCdaRequest)
+      throws TransformerException, IOException
+  {
+    patientXMLObserver.onNext(inCdaRequest);
+  }
+
+  @RequestMapping(path = "{id}/body.html", method = RequestMethod.GET,
+                  produces = MediaType.TEXT_HTML_VALUE)
+  public ResponseEntity<String> getHtml(@PathVariable("id") long id) {
+    Patient patient = patientRepository.findOne(id);
+    if (patient != null)
+    {
+      String body = patient.body;
+      if (body != null)
+      {
+        return ResponseEntity.ok(body);
+      }
     }
+    return ResponseEntity.notFound().build();
+  }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity<Resources<Resource<Patient>>> findAll() {
-        LOGGER.info("Finding patients");
-        return ResponseEntity.ok(Resources.wrap(repository.findAll()));
-    }
 }
