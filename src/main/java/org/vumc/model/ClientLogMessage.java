393a5595a8a7b8cb29513bb5654bb3c7eab1241d
yarbrojl <yarbrojl@b9cdbc32-ae21-0410-9a0e-be68b19515d5>
2017-04-05 17:16:33 +0000

    Added Logging
    git-svn-id: https://vcs.mc.vanderbilt.edu/health-it/continuum/spring/trunk@24700 b9cdbc32-ae21-0410-9a0e-be68b19515d5

---
+++ src/main/java/org/vumc/model/ClientLogMessage.java
@@ -0,0 +1,17 @@
+/* Project: continuum
+ * File: ClientLogMessage.java
+ * Created: Apr 04, 2017
+ * Author: Josh Yarbrough - josh.yarbrough@vanderbilt.edu
+ *
+ * This code is copyright (c) 2017 Vanderbilt University Medical Center
+ */
+package org.vumc.model;
+
+
+public class ClientLogMessage {
+
+    public String level;
+    public String msg;
+    public String mrn;
+
+}
