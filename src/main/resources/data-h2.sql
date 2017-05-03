18abd522a8cbb212b665b6af3198483b6ab01f5b
bernerdc <bernerdc@b9cdbc32-ae21-0410-9a0e-be68b19515d5>
2017-05-03 16:12:29 +0000

    More unit tests
    git-svn-id: https://vcs.mc.vanderbilt.edu/health-it/continuum/spring/trunk@25603 b9cdbc32-ae21-0410-9a0e-be68b19515d5

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -3,7 +3,7 @@ create table message_receipt (server_node_identifier varchar(50) not null, messa
 alter table message_receipt add constraint fk_message_receipt_message foreign key (message_id) references message;
 
 -- noinspection SqlNoDataSourceInspectionForFiles
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '<html><head><link href="https://www.orionhealth.com/software/ccd/style/skin_v2.4.css" rel="stylesheet" type="text/css"></link><title>Continuity of Care Document</title></head><body><h2 align="center">Continuity of Care Document</h2><p align="center"><b>Created On: </b>March 13, 2017</p><hr></hr><div class="header"><div class="demographics sticky"><div class="bl"><div class="br"><div class="tr"><div class="person-name">HOOT, SCOOT </div><div class="sex-age"><span id="calculatedAge"></span></div><div class="id">AACJ-4531-1<span class="label">
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '<html><head><link href="https://www.orionhealth.com/software/ccd/style/skin_v2.4.css" rel="stylesheet" type="text/css"></link><title>Continuity of Care Document</title></head><body><h2 align="center">Continuity of Care Document</h2><p align="center"><b>Created On: </b>March 13, 2017</p><hr></hr><div class="header"><div class="demographics sticky"><div class="bl"><div class="br"><div class="tr"><div class="person-name">HOOT, SCOOT </div><div class="sex-age"><span id="calculatedAge"></span></div><div class="id">AACJ-4531-1<span class="label">
  (ORION)
  </span><br></br><br></br></div></div></div></div></div></div><div class="section"><b>Electronically generated</b><b> by </b>Vanderbilt Health Affiliated Network<b> on </b>March 13, 2017</div><div class="section"><span><a name="N65879" href="#toc"><h2>Alerts, Allergies and Adverse Reactions</h2></a></span></div>
 
@@ -58,9 +58,9 @@ INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, C
  ageWithSeparator = ageWithSeparator + '', '';
  }
  document.getElementById(''calculatedAge'').innerHTML = ageWithSeparator;
- </script></html>', null, null, 'HOOT', 'SCOOT', null, 'AACJ-4531-1', current_timestamp-100);
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '', TO_DATE('1967-04-10 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'M', 'BAGGINS', 'FRODO', null, '1000050', current_timestamp-50);
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '<html><head><link href="http://www.orionhealth.com/software/ccd/style/skin_v2.4.css" rel="stylesheet" type="text/css"></link><title>Continuity of Care Document</title></head><body><h2 align="center">Continuity of Care Document</h2><p align="center"><b>Created On: </b>March 13, 2017</p><hr></hr><div class="header"><div class="demographics sticky"><div class="bl"><div class="br"><div class="tr"><div class="person-name">HOOT, SCOOT </div><div class="sex-age"><span id="calculatedAge"></span></div><div class="id">AACJ-4531-1<span class="label">
+ </script></html>', null, null, 'HOOT', 'SCOOT', null, 'AACJ-4531-1', 'Test', current_timestamp-100);
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '', TO_DATE('1967-04-10 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'M', 'BAGGINS', 'FRODO', null, '1000050', 'test', current_timestamp-50);
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '<html><head><link href="http://www.orionhealth.com/software/ccd/style/skin_v2.4.css" rel="stylesheet" type="text/css"></link><title>Continuity of Care Document</title></head><body><h2 align="center">Continuity of Care Document</h2><p align="center"><b>Created On: </b>March 13, 2017</p><hr></hr><div class="header"><div class="demographics sticky"><div class="bl"><div class="br"><div class="tr"><div class="person-name">HOOT, SCOOT </div><div class="sex-age"><span id="calculatedAge"></span></div><div class="id">AACJ-4531-1<span class="label">
  (ORION)
  </span><br></br><br></br></div></div></div></div></div></div><div class="section"><b>Electronically generated</b><b> by </b>Vanderbilt Health Affiliated Network<b> on </b>March 13, 2017</div><div class="section"><span><a name="N65879" href="#toc"><h2>Alerts, Allergies and Adverse Reactions</h2></a></span></div>
 
@@ -115,9 +115,9 @@ INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, C
  ageWithSeparator = ageWithSeparator + '', '';
  }
  document.getElementById(''calculatedAge'').innerHTML = ageWithSeparator;
- </script></html>', null, null, 'HOOT', 'SCOOT', null, 'AACJ-4531-1', current_timestamp-.5);
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '', TO_DATE('1967-04-09', 'YYYY-MM-DD'), 'M', 'BAGGINS', 'BILBO', null, '1000060', current_timestamp-.4);
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '', TO_DATE('2017-04-10', 'YYYY-MM-DD'), 'F', 'FLINTSTONE', 'WILMA', null, '1000070', current_timestamp-.6);
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '', current_timestamp-2, 'F', 'FLINTSTONE', 'PEBBLES', null, '1000080', current_timestamp-.2);
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '', current_timestamp-59, 'F', 'RUBBLE', 'BARNEY', null, '1000085', current_timestamp);
-INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, CREATED) VALUES (nextval('PATIENT_SEQ'), '', current_timestamp-1, 'U', 'FLINTSTONE', 'DINO', null, '1000090', current_timestamp-.7);
+ </script></html>', null, null, 'HOOT', 'SCOOT', null, 'AACJ-4531-1', 'test', current_timestamp-.5);
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '', TO_DATE('1967-04-09', 'YYYY-MM-DD'), 'M', 'BAGGINS', 'BILBO', null, '1000060', 'test', current_timestamp-.4);
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '', TO_DATE('2017-04-10', 'YYYY-MM-DD'), 'F', 'FLINTSTONE', 'WILMA', null, '1000070', 'test', current_timestamp-.6);
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '', current_timestamp-2, 'F', 'FLINTSTONE', 'PEBBLES', null, '1000080', 'test', current_timestamp-.2);
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '', current_timestamp-59, 'F', 'RUBBLE', 'BARNEY', null, '1000085', 'test', current_timestamp);
+INSERT INTO PATIENT (ID, BODY, DOB, GENDER, FAMILY, GIVEN, SUFFIX, PATIENT_ID, ID_ASSIGNING_AUTHORITY, CREATED) VALUES (nextval('PATIENT_SEQ'), '', current_timestamp-1, 'U', 'FLINTSTONE', 'DINO', null, '1000090', 'test', current_timestamp-.7);
